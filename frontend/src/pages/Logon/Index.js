e5205aeb0987e68a9fdc3faed27fde5588ebbee1
Rodrigo Fregate Baraldi <baraldi@MacBook-Pro-de-Rodrigo.local>
2020-03-26 13:58:32 -0300

    Pagina Login 4344 tempo video

--- frontend/src/pages/Logon/Index.js
+++ frontend/src/pages/Logon/Index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FiLogIn} from 'react-icons/fi'; //feather icons
 
 import './styles.css';
 
 import  heroesImg from '../../assets/heroes.png';
+import  logoImg from '../../assets/logo.svg';
 
 
 export default function Logon(){
@@ -10,9 +12,19 @@ export default function Logon(){
 return(
 
   <div className="logon-container">
-    <section className="form">
-     
-    </section>
+<section className="form">
+    <img src={logoImg} alt="Be The Hero" />
+
+    <form>
+        <h1>Faça o seu Logon</h1>
+        <input placeholder="Sua ID" />
+        <button className="button" type="submit">Entrar</button>
+   
+   <a href="/register"><FiLogIn size='14' color='#E02041'   />Não tenho cadastro</a>
+   
+   
+    </form>
+</section>
     <img src={heroesImg} alt="Heroes" />
   </div>
 );

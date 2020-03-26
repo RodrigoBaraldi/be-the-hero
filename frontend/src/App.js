fbf7b243f9fe01f194b65744e1e2925e441eb090
Rodrigo Fregate Baraldi <baraldi@MacBook-Pro-de-Rodrigo.local>
2020-03-26 11:36:43 -0300

    Header com cor Azul

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
-import React, {useState} from 'react';
-import Header from './Header';
+import React from 'react';
+import Logon from './pages/Logon/Index';
 
 
 
 function App() {
  
-  const [counter, setCounter] = useState(0);
-
-  function increment(){
-  setCounter(counter + 1);
-  }
+  
  
  
  
 return (
 <div>
-  <Header>Contador: {counter}  </Header>
-  <button onClick={increment}>Incrementar</button>
+
+<Logon/>
+  
 </div>
   );
 }

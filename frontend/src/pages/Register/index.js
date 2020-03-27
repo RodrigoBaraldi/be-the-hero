632e2410570d262cbb2f6e45cc5a81058b7779b9
Rodrigo Fregate Baraldi <baraldi@MacBook-Pro-de-Rodrigo.local>
2020-03-26 23:31:45 -0300

    video01h23m cadastrando ONG

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,54 @@
-import React from  'react';
+import React, {useState} from  'react';
+
 import { FiArrowLeft} from 'react-icons/fi'; //feather icons
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import  logoImg from '../../assets/logo.svg';
 
 export default function Register(){
+
+const[name, setName]=useState('');
+const[email, setEmail]=useState('');
+const[whatsapp, setWhatsaapp]=useState('');
+const[city, setCity]=useState('');
+const[uf, setUf]=useState('');
+
+const history=useHistory();
+
+
+
+async function handleRegister(e){
+
+  
+    e.preventDefault();
+
+    const data={
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+    }
+
+    try {
+    const response = await api.post('/ongs', data);
+
+    alert(`O Id de sua ONG é ${response.data.id}`);
+
+    history.push('/'); 
+    
+    } catch(err){
+        alert(err);
+    }
+}
+
+
+
+
 
 return (
 
@@ -24,15 +66,38 @@ return (
         
         </section>
     
-    <form>
+    <form onSubmit={handleRegister}>
 
-    <input placeholder="Nome da Ong"/> 
-    <input type="email" placeholder="E-mail"/>
-    <input placeholder="WhatsApp" />
+    <input 
+    placeholder="Nome da Ong"
+    value={name}
+    onChange={e=>setName(e.target.value)}
+    />
+
+
+    <input
+    type="email" 
+    placeholder="E-mail"
+    value={email}
+    onChange={e=>setEmail(e.target.value)}
+    />
+
+
+    <input 
+    placeholder="WhatsApp" 
+    value={whatsapp}
+    onChange={e=>setWhatsaapp(e.target.value)}
+    />
 
     <div className="input-group">
-        <input placeholder="Cidade"/>
-        <input placeholder="UF"  style={{ width:80}}   />
+        <input placeholder="Cidade"
+        value={city}
+        onChange={e=>setCity(e.target.value)}
+        />
+        <input placeholder="UF"  style={{ width:80}}
+        value={uf}
+        onChange={e=>setUf(e.target.value)}
+        />
     </div>
 
 <button className="button" type="submit">Cadastrar</button>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter,Switch,Route } from 'react-router-dom';
+
+import { BrowserRouter, Switch,  Route } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
